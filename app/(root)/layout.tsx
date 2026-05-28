7611696cd031c1e6fb0600cc554dd63ba82d16d0
workaditya704 <workaditya704@gmail.com>
2026-05-28 13:55:30 +0530

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="h-screen text-gray-400">
+      {/* <Header /> */}
+      <div className="container py-10 mx-auto">{children}</div>
+    </main>
+  );
+};
+
+export default Layout;
