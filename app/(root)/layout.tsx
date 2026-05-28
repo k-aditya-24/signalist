ed148c5f196588a2d741fa9164d169e3aa35ad07
workaditya704 <workaditya704@gmail.com>
2026-05-28 16:40:43 +0530

    implement header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/Header/Header";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="h-screen text-gray-400">
-      {/* <Header /> */}
+      <Header/>
       <div className="container py-10 mx-auto">{children}</div>
     </main>
   );
